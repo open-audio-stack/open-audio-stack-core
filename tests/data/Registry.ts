d3f763170afb6bf4c59fca95191e0bf083340cde
Kim T <kimturley@gmail.com>
2024-10-29 22:57:09 -0700

    Update registry url

--- tests/data/Registry.ts
+++ tests/data/Registry.ts
@@ -3,6 +3,6 @@ import { RegistryInterface } from '../../src/types/Registry';
 export const REGISTRY: RegistryInterface = {
   name: 'Open Audio Registry',
   packages: {},
-  url: 'https://openaudio.github.io/registry',
+  url: 'https://open-audio-stack.github.io/open-audio-stack-registry',
   version: '1.0.0',
 };
